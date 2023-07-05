3ab31812b87a59d17254003b908127f0168497a7
Jericho Aquino <yeoligoakino@gmail.com>
2023-07-05 16:38:33 +0000

    the main router

---
+++ html/public/index.php
@@ -0,0 +1,49 @@
+<?php
+
+// show all errors
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+
+ini_set('memory_limit', '256M');
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use eru123\router\Router;
+use eru123\router\Context;
+use eru123\config\DotEnv;
+
+DotEnv::load(__DIR__ . '/../');
+
+$router = new Router();
+$router->error(function (Throwable $e) {
+    // $code = intval($e->getCode() ?: 500);
+    $code = 500;
+    return Router::status_page($code, $code . ' Internal Server Error', 'The server encountered an internal error and was unable to complete your request. Either the server is overloaded or there is an error in the application.');
+    // return Router::status_page($code, $code . ' Internal Server Error', $e->getMessage());
+});
+$router->get('/info', function (Context $c) {
+    phpinfo();
+    exit;
+});
+
+$router->static('/', __DIR__, [], function (Context $c) {
+    if ($c->file == '/index.php' || substr($c->file, -4) == '.php') {
+        return false;
+    }
+});
+
+$router->post('/cdn/upload', 'App\Controller\CDN@upload');
+$router->get('/cdn/stream/$id', 'App\Controller\CDN@stream');
+$router->get('/cdn/stream/$id/$name', 'App\Controller\CDN@stream');
+$router->get('/cdn/download/$id', 'App\Controller\CDN@download');
+$router->get('/cdn/download/$id/$name', 'App\Controller\CDN@download');
+
+// if server name is cdn.jericho.work
+if (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'cdn.jericho.work') {
+    $router->get('/stream/$id', 'App\Controller\CDN@stream');
+    $router->get('/stream/$id/$name', 'App\Controller\CDN@stream');
+    $router->get('/download/$id', 'App\Controller\CDN@download');
+    $router->get('/download/$id/$name', 'App\Controller\CDN@download');
+}
+
+$router->run();

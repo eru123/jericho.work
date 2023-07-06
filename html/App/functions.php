876aa46a6815355458ff398ba04c94e8b69f00bf
eru123 <yeoligoakino@gmail.com>
2023-07-06 18:14:41 +0800

    added get ip

--- html/App/functions.php
+++ html/App/functions.php
@@ -17,3 +17,12 @@ function cdn_download(string $id, string $name = null)
     $cdn_url = env('CDN_URL', 'http://localhost/cdn');
     return rtrim($cdn_url, '/') . "/download/$id" . ($name ? "/$name" : '');
 }
+
+function get_ip()
+{
+    $ip = $_SERVER['REMOTE_ADDR'] ?? null;
+    if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['f'];
+    }
+    return $ip ?? '0';
+}

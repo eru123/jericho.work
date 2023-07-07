3350b16df61b92c506f67c581fc20040b5a10141
eru123 <yeoligoakino@gmail.com>
2023-07-07 10:02:12 +0800

    configure vite config for dev and prod

---
+++ html/client/cdn/vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 3000,
+    strictPort: true
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  publicDir: 'public',
+  base: './',
+  build: {
+    manifest: true,
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsDir: '__',
+    rollupOptions: {
+      input: {
+        main: 'src/main.js'
+      }
+    }
+  },
+})

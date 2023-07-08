a9c2861790d97beaeaf44862ac57faaf9289b505
eru123 <yeoligoakino@gmail.com>
2023-07-08 17:02:54 +0800

    fix modal

--- html/client/cdn/src/App.vue
+++ html/client/cdn/src/App.vue
@@ -1,34 +1,25 @@
 <script setup>
 
-// define a ref for the file input
 import { ref } from 'vue'
 const fileInput = ref(null)
 const uploading = ref(false)
 
 const modalRef = ref(null)
-// function to create a referenced modal
+const modalTitle = ref('')
+const modalMessage = ref('')
+const modalCloseCallback = ref(null)
+const files = ref([])
+
 const createModal = (title, message, callback = null) => {
-  const modal = document.createElement('div')
-  modal.className = 'modal'
-  modal.innerHTML = `
-    <div class="modal-content">
-      <h1>${title}</h1>
-      <p>${message}</p>
-      <button class="modal-close">Close</button>
-    </div>
-  `
-  modalRef.value = modal
-  document.body.appendChild(modal)
-  modal.querySelector('.modal-close').addEventListener('click', () => {
-    modalRef.value = null
-    document.body.removeChild(modal)
-    if (callback) {
-      callback()
-    }
-  })
+  modalTitle.value = title
+  modalMessage.value = message
+  modalRef.value.classList.add('active')
+
+  if (callback) {
+    modalCloseCallback.value = callback
+  }
 }
 
-// on file input change, upload file to server
 const onFileChange = () => {
   if (!fileInput.value.files.length) {
     return
@@ -43,12 +34,15 @@ const onFileChange = () => {
   })
     .then((e) => e.json())
     .then((e) => {
-      console.log(e)
+      if (!e?.status) {
+        throw (e?.error || 'Something went wrong! Please try again later.')
+      }
+
+      files.value = [...e.files, ...files.value]
     })
     .catch((e) => {
-      console.error(e)
-      // create a modal to show the error
-      createModal('Error', e.message)
+      console.log('xxx', e)
+      createModal('Error', e)
     })
     .finally(() => {
       uploading.value = false
@@ -60,31 +54,124 @@ const upload = () => {
   fileInput.value.click()
 }
 
+const modalClose = () => {
+  modalRef.value.classList.remove('active')
+  modalRef.value.classList.add('hidden')
+  if (modalCloseCallback.value) {
+    modalCloseCallback.value()
+  }
+
+  modalCloseCallback.value = null
+}
+
 </script>
 
 <template>
   <div>
     <h1>OpenCDN</h1>
-    <h2>Upload a file</h2>
     <input type="file" ref="fileInput" style="display: none" @change="onFileChange" />
-    <button @click="upload" :disabled="uploading">Upload</button>
-    <div v-if="uploading">Uploading...</div>
+    <button @click="upload" :disabled="uploading">Upload a file</button>
+
+    <div v-if="files.length" class="files">
+      <h2>Uploaded files</h2>
+      <div class="file" v-for="f in files" :key="f.id">
+        <div>
+          
+        </div>
+      </div>
+    </div>
+
+    <div v-if="uploading" class="uploading-dialog">
+      <h2>Uploading...</h2>
+      <progress></progress>
+    </div>
   </div>
+  <Teleport to="body">
+    <div ref="modalRef" class="modal hidden">
+      <div class="modal-content">
+        <h2>{{ modalTitle }}</h2>
+        <p>{{ modalMessage }}</p>
+        <div class="actions" align="right">
+          <button ref="modalClose" class="close-modal" @click="modalClose">Close</button>
+        </div>
+      </div>
+    </div>
+  </Teleport>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.uploading-dialog {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.8);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  z-index: 100;
 }
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  z-index: 100;
+  background: rgba(0, 0, 0, 0.6);
+  padding: .5rem;
 }
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.modal.active {
+  display: flex;
+}
+
+.hidden {
+  display: none;
+}
+
+.modal-content {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
+  max-width: 100%;
+  width: 300px;
+}
+
+.modal-content h2 {
+  margin: 0;
+  color: #333;
+  font-size: 1.5rem;
+}
+
+.modal-content p {
+  margin: 0;
+  color: #444;
+  font-size: 1rem;
+}
+
+.modal-close {
+  padding: .25rem .5rem;
+  border-radius: .25rem;
+  background: #dddddd;
+  color: #333;
+  transition: all .2s ease;
+  border: none;
+}
+
+.modal-close:hover {
+  background: #bbb;
+}
+
+.actions {
+  margin-top: 1rem;
 }
 </style>

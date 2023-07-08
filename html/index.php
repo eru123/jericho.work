14e5c98e1d9be5f92611ebe10dca3ede563fea7d
eru123 <yeoligoakino@gmail.com>
2023-07-08 09:24:20 +0800

    move index.php

--- html/index.php
+++ html/index.php
@@ -4,17 +4,13 @@
 // error_reporting(E_ALL);
 // ini_set('display_errors', 1);
 
-ini_set('memory_limit', '256M');
-
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 use eru123\router\Router;
 use eru123\router\Context;
 use eru123\config\DotEnv;
-use eru123\helper\ArrayUtil as A;
-use eru123\helper\Format;
 
-DotEnv::load(__DIR__ . '/../');
+DotEnv::load(__DIR__);
 
 $router = new Router();
 $router->error(function (Throwable $e) {
@@ -34,41 +30,48 @@ $router->error(function (Throwable $e) {
     //     'code' => $e->getCode(),
     // ];
 });
+
 $router->get('/info', function (Context $c) {
     phpinfo();
     exit;
 });
 
-$router->static('/', __DIR__, [], function (Context $c) {
-    // throw new Exception('404 Not Found sdfsdf', 404);
+$router->static('/', __DIR__ . '/public', [], function (Context $c) {
     if ($c->file == '/index.php' || substr($c->file, -4) == '.php') {
         return false;
     }
 });
 
-vite($router, '/cdn', true, [
-    'entry' => 'src/main.js',
-    'client' => 'http://127.0.0.1:3000',
-    'public' => __DIR__ . '/../client/cdn/public',
-    'src' => __DIR__ . '/../client/cdn/src',
-    'dist' => __DIR__ . '/../client/cdn/dist',
-    'favicon' => 'favicon.ico',
-]);
-
 // if server name is cdn.jericho.work
 if (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == env('CDN_DOMAIN')) {
-
-
     // $router->get('/stream/$id', 'App\Controller\CDN@stream');
     $router->get('/stream/$id/$name', 'App\Controller\CDN@stream');
     // $router->get('/download/$id', 'App\Controller\CDN@download');
     $router->get('/download/$id/$name', 'App\Controller\CDN@download');
+
+    vite($router, '/', true, [
+        'entry' => 'src/main.js',
+        'client' => 'http://127.0.0.1:3000',
+        'public' => __DIR__ . '/client/cdn/public',
+        'src' => __DIR__ . '/client/cdn/src',
+        'dist' => __DIR__ . '/client/cdn/dist',
+        'favicon' => 'favicon.ico',
+    ]);
 } else {
     $router->post('/cdn/upload', 'App\Controller\CDN@upload');
     // $router->get('/cdn/stream/$id', 'App\Controller\CDN@stream');
     $router->get('/cdn/stream/$id/$name', 'App\Controller\CDN@stream');
     // $router->get('/cdn/download/$id', 'App\Controller\CDN@download');
     $router->get('/cdn/download/$id/$name', 'App\Controller\CDN@download');
+
+    vite($router, '/cdn', true, [
+        'entry' => 'src/main.js',
+        'client' => 'http://127.0.0.1:3000',
+        'public' => __DIR__ . '/client/cdn/public',
+        'src' => __DIR__ . '/client/cdn/src',
+        'dist' => __DIR__ . '/client/cdn/dist',
+        'favicon' => 'favicon.ico',
+    ]);
 }
 
 

8ba7d17c1b01b6e2665188fade28f79b5405410e
eru123 <yeoligoakino@gmail.com>
2023-07-08 21:00:36 +0800

    update

--- html/client/cdn/src/App.vue
+++ html/client/cdn/src/App.vue
@@ -28,6 +28,7 @@ const onFileChange = () => {
   const formData = new FormData()
   formData.append('file', file)
   uploading.value = true
+
   fetch('/upload', {
     method: 'POST',
     body: formData
@@ -39,9 +40,9 @@ const onFileChange = () => {
       }
 
       files.value = [...e.files, ...files.value]
+      createModal('Success!', e?.message || 'File uploaded successfully.')
     })
     .catch((e) => {
-      console.log('xxx', e)
       createModal('Error', e)
     })
     .finally(() => {
@@ -64,26 +65,75 @@ const modalClose = () => {
   modalCloseCallback.value = null
 }
 
+const copy = (text) => {
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      createModal('Copied!', 'The URL has been copied to your clipboard.')
+    })
+    .catch(() => {
+      createModal('Error', 'Something went wrong! Please try again later.')
+    })
+}
+
+const hsize = (bytes) => {
+  const i = Math.floor(Math.log(bytes) / Math.log(1024))
+  return `${(bytes / Math.pow(1024, i)).toFixed(2)} ${['B', 'KB', 'MB', 'GB', 'TB'][i]}`
+}
+
 </script>
 
 <template>
   <div>
-    <h1>OpenCDN</h1>
+    <h1 class="brand">Open<span class="cdn">CDN</span></h1>
     <input type="file" ref="fileInput" style="display: none" @change="onFileChange" />
     <button @click="upload" :disabled="uploading">Upload a file</button>
 
     <div v-if="files.length" class="files">
       <h2>Uploaded files</h2>
       <div class="file" v-for="f in files" :key="f.id">
-        <div>
-          
+        <div v-if="f.uploaded">
+          <h3 class="file-header">
+            <div class="file-name">{{ f.file.name }}<br /><small>{{ hsize(f.file.size) }}</small></div>
+            <div class="file-actions">
+              <a :href="f.stream" target="_blank">Stream</a>
+              <a :href="f.download" target="_blank">Download</a>
+            </div>
+          </h3>
+          <div class="file-copies">
+
+            <div class="file-copy">
+              <span class="file-copy-label">
+                Stream URL
+              </span>
+              <div class="file-copy-data">
+                <input type="text" :value="f.stream" readonly />
+                <button @click="copy(f.stream)">Copy</button>
+              </div>
+            </div>
+
+            <div class="file-copy">
+              <span class="file-copy-label">
+                Download URL
+              </span>
+              <div class="file-copy-data">
+                <input type="text" :value="f.download" readonly />
+                <button @click="copy(f.download)">Copy</button>
+              </div>
+            </div>
+
+            <div class="file-copy" v-if="f?.html?.tag">
+              <span class="file-copy-label">
+                HTML Tag
+              </span>
+              <div class="file-copy-data">
+                <input type="text" :value="f.html.tag" readonly />
+                <button @click="copy(f.html.tag)">Copy</button>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
-    </div>
-
-    <div v-if="uploading" class="uploading-dialog">
-      <h2>Uploading...</h2>
-      <progress></progress>
     </div>
   </div>
   <Teleport to="body">
@@ -96,14 +146,147 @@ const modalClose = () => {
         </div>
       </div>
     </div>
+    <div v-show="uploading" class="uploading-dialog">
+      <h2>Uploading...</h2>
+      <progress></progress>
+    </div>
+  </Teleport>
+  <Teleport to="body">
+    
   </Teleport>
 </template>
 
 <style scoped>
+.files {
+  max-width: 800px;
+  width: 100%;
+  margin: 2rem auto 0 auto;
+  padding: 0 1rem 2rem 1rem;
+}
+
+.files>h2 {
+  margin: 0;
+  font-size: .9rem;
+  color: #cfcfcf;
+  text-align: left;
+  text-transform: uppercase;
+}
+
+.file {
+  margin-top: 1rem;
+  border-radius: 5px;
+  border: 0;
+  padding: 1rem;
+  background-color: #121216;
+}
+
+.file-header {
+  display: flex;
+  flex-direction: column;
+}
+
+.file-name {
+  font-size: 1.25rem;
+  color: #fafafa;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 100%;
+  margin-right: 1rem;
+}
+
+.file-name small {
+  font-size: .75rem;
+  color: #cfcfcf;
+}
+
+.file-actions {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 1rem;
+  gap: .5rem;
+}
+
+.file-actions a {
+  padding: .25rem 1rem;
+  font-size: 1rem;
+  border-radius: .25rem;
+  background: #006650;
+  color: #fff;
+}
+
+.file-actions a:hover {
+  background: #00503f;
+}
+
+.file-copies {
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
+}
+
+.file-copy {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1rem;
+}
+
+.file-copy-label {
+  font-size: 1rem;
+  text-align: left;
+  color: #cfcfcf;
+}
+
+.file-copy-data {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: .5rem;
+}
+
+.file-copy-data input[readonly] {
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: .25rem .5rem;
+  font-size: 1rem;
+  width: 100%;
+  background: #fafafa;
+  color: #333;
+}
+
+.file-copy-data button {
+  padding: .25rem .5rem;
+  border-radius: .25rem;
+  transition: all .2s ease;
+  border: none;
+  background: #006650;
+  color: #fff;
+}
+
+.file-copy-data button:hover {
+  background: #00503f;
+}
+
+.brand {
+  font-size: 3rem;
+  font-weight: 700;
+  margin: 2rem 2rem 2rem 1rem;
+  color: #fff;
+  user-select: none;
+}
+
+.brand .cdn {
+  color: #006650;
+}
+
 .uploading-dialog {
   position: fixed;
   top: 0;
   left: 0;
+  right: 0;
+  bottom: 0;
   width: 100vw;
   height: 100vh;
   background: rgba(0, 0, 0, 0.8);
@@ -118,6 +301,8 @@ const modalClose = () => {
   position: fixed;
   top: 0;
   left: 0;
+  right: 0;
+  bottom: 0;
   width: 100vw;
   height: 100vh;
   display: flex;
@@ -126,7 +311,7 @@ const modalClose = () => {
   flex-direction: column;
   z-index: 100;
   background: rgba(0, 0, 0, 0.6);
-  padding: .5rem;
+  padding: 0;
 }
 
 .modal.active {
@@ -142,8 +327,8 @@ const modalClose = () => {
   padding: 20px;
   border-radius: 10px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
-  max-width: 100%;
-  width: 300px;
+  max-width: 300px;
+  width: 100%;
 }
 
 .modal-content h2 {

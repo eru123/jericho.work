a82f1b54ee17f2677f573c88c243bf3ba980cf24
eru123 <yeoligoakino@gmail.com>
2023-07-10 15:20:35 +0800

    added install script and use pnpm

--- html/gulpfile.js
+++ html/gulpfile.js
@@ -1,13 +1,18 @@
 const gulp = require('gulp');
 const spawn = require('child_process').spawn;
 
+gulp.task('cdn-install', function(done) {
+  spawn('pnpm', ['install'], { cwd: '/var/www/html/client/cdn' }).on('close', done);
+});
+
 gulp.task('cdn-dev', function(done) {
-  spawn('npm', ['run', 'dev'], { cwd: '/var/www/html/client/cdn' }).on('close', done);
+  spawn('pnpm', ['run', 'dev'], { cwd: '/var/www/html/client/cdn' }).on('close', done);
 });
 
 gulp.task('cdn-build', function(done) {
-    spawn('npm', ['run', 'build'], { cwd: '/var/www/html/client/cdn' }).on('close', done);
+    spawn('pnpm', ['run', 'build'], { cwd: '/var/www/html/client/cdn' }).on('close', done);
 });
 
+gulp.task('install', gulp.parallel('cdn-install'));
 gulp.task('dev', gulp.parallel('cdn-dev'));
 gulp.task('build', gulp.parallel('cdn-build'));

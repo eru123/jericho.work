0117f5dbd8cd5ced3ddf12c495e9604a00a10f3d
eru123 <yeoligoakino@gmail.com>
2023-07-14 17:44:24 +0800

    added ButtonLink component

---
+++ html/client/cdn/src/components/ButtonLink.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+    href: String,
+    text: String
+})
+</script>
+
+<template>
+    <a :href="href" target="_blank">{{text}}</a>
+</template>
+<style scoped>
+a {
+  padding: .25rem 1rem;
+  font-size: 1rem;
+  border-radius: .25rem;
+  background: #006650;
+  color: #fff;
+}
+
+a:hover {
+  background: #00503f;
+}
+</style>

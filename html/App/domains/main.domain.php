cd5124b7de051ecc76d620a79375cd522dae0e8f
eru123 <yeoligoakino@gmail.com>
2023-07-15 11:05:01 +0800

    update main domain

--- html/App/domains/main.domain.php
+++ html/App/domains/main.domain.php
@@ -4,18 +4,18 @@ use eru123\router\Router;
 
 $router = new Router();
 
+// $router->get('/info', function () {
+    // phpinfo();
+    // exit;
+// });
+
 $router->get('/', function () {
     Router::status_page(200, 'Welcome!', 'Hi there! Thanks for visiting, the website is currently under construction, please come back later.');
 });
 
-$router->get('/info', function () {
-    phpinfo();
-    exit;
-});
-
 $public_dir = realpath(__DIR__ . '/../../public');
 if ($public_dir && is_dir($public_dir)) {
-    $router->static('/', [__DIR__ . '/../../public'], ['index.php', 'index.html']);
+    $router->static('/', [$public_dir], ['index.php', 'index.html']);
 }
 
 return $router;

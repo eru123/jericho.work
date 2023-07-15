31fe9aaecb61fd3718744c01375ca11db2858806
eru123 <yeoligoakino@gmail.com>
2023-07-15 11:06:08 +0800

    added cf dev mode stat

---
+++ html/scripts/cf_dev_mode_status.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/autoload.php';
+use App\Plugin\CF;
+
+$msg = CF::dev_mode() ? 'Cloudflare dev mode ON' : 'Cloudflare dev mode OFF';
+echo $msg, PHP_EOL;
+
+writelog($msg);

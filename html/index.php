dab2adbbc3de10199850ba95b3fcee16adc1d99b
eru123 <yeoligoakino@gmail.com>
2023-07-15 11:10:48 +0800

    update project entry

--- html/index.php
+++ html/index.php
@@ -1,18 +1,12 @@
 <?php
 
-// show all errors
-// error_reporting(E_ALL);
-// ini_set('display_errors', 1);
+require_once __DIR__ . '/autoload.php';
 
-require_once __DIR__ . '/vendor/autoload.php';
-
-use eru123\config\DotEnv;
 use App\Plugin\Domain;
-
-DotEnv::load(__DIR__);
 
 $domain = Domain::instance(__DIR__ . '/App/domains');
 $domain->createVirtualHost(env('DOMAIN'), 'main');
 $domain->createVirtualHost(env('CDN_DOMAIN'), 'cdn');
+$domain->createVirtualHost(env('ADMIN_DOMAIN'), 'admin');
 
 $domain->serve();

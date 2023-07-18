d42281c39c4bdc54e01752395b71c6a7b2d4cd9f
eru123 <yeoligoakino@gmail.com>
2023-07-18 16:41:57 +0800

    implement vuetify

--- html/client/admin/src/main.js
+++ html/client/admin/src/main.js
@@ -1,8 +1,22 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import router from './router'
+import './style.css'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+        defaultTheme: 'dark',
+    },
+})
+
 app.use(router)
+app.use(vuetify)
 app.mount('#app')

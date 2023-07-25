d0ae8c88d1509272548ebdb891bed9fe9a70a4fa
eru123 <yeoligoakino@gmail.com>
2023-07-25 18:14:35 +0800

    update

--- html/App/domains/main.domain.php
+++ html/App/domains/main.domain.php
@@ -5,12 +5,22 @@ use eru123\router\Router;
 $router = new Router();
 
 // $router->get('/info', function () {
-    // phpinfo();
-    // exit;
+// phpinfo();
+// exit;
 // });
 
-$router->get('/', function () {
-    Router::status_page(200, 'Welcome!', 'Hi there! Thanks for visiting, the website is currently under construction, please come back later.');
+$alternative_links = [
+    'OpenCDN' => '/cdn',
+];
+
+$router->get('/', function () use ($alternative_links) {
+    $msg = 'Hi there! Thanks for visiting,<br /> the website is currently under construction, please come back later. <br /><br />';
+    $msg .= 'In the meantime, you can visit the our other services that are working: '. implode(', ', array_map(function ($name, $link) {
+        $name = htmlspecialchars($name);
+        $link = htmlspecialchars($link);
+        return "<a href=\"$link\">$name</a>";
+    }, array_keys($alternative_links), $alternative_links));
+    Router::status_page(200, 'Welcome!', $msg);
 });
 
 $public_dir = realpath(__DIR__ . '/../../public');
@@ -21,7 +31,9 @@ if ($public_dir && is_dir($public_dir)) {
 
 $cdn = require __APP__  . '/routes/cdn.php';
 $admin = require __APP__  . '/routes/admin.php';
+$api = require __APP__  . '/routes/api.php';
 
 $router->child($cdn);
 $router->child($admin);
+$router->child($api);
 return $router;

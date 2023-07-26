f1214c0c0108b9adbd7e0a8f108f35db606fcfde
eru123 <yeoligoakino@gmail.com>
2023-07-26 15:05:03 +0800

    update portfolio

--- html/domains/jericho-portfolio.domain.php
+++ html/domains/jericho-portfolio.domain.php
@@ -3,6 +3,8 @@
 use eru123\router\Router;
 
 $portfolio = new Router();
-$portfolio->static('/', [__DIR__ . '/../../client/admin/dist/admin'], [], 'App\Controller\Admin::index');
+$portfolio->get('/', function () {
+    Router::status_page(200, 'Welcome!', 'Hi there! This is jericho-portfolio, a portfolio website for Jericho. <br /> Currently, this website is under construction, please come back later.');
+});
 
 return $portfolio;

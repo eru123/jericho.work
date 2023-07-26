76e7e798479bcec7c7f4e2ff7196d6c758e572d4
eru123 <yeoligoakino@gmail.com>
2023-07-26 14:58:45 +0800

    added jericho portfolio

--- html/index.php
+++ html/index.php
@@ -4,9 +4,10 @@ require_once __DIR__ . '/autoload.php';
 
 use App\Plugin\Domain;
 
-$domain = Domain::instance(__DIR__ . '/App/domains');
+$domain = Domain::instance(__DOMAINS__);
 $domain->createVirtualHost(env('DOMAIN'), 'main');
 $domain->createVirtualHost(env('CDN_DOMAIN'), 'cdn');
 $domain->createVirtualHost(env('ADMIN_DOMAIN'), 'main');
+$domain->createVirtualHost(env('JERICHO_PORTFOLIO_DOMAIN'), 'jericho-portfolio');
 
 $domain->serve();

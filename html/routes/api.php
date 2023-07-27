465cd8b79d91977b634e93edb09369fe9991468b
eru123 <yeoligoakino@gmail.com>
2023-07-27 17:57:12 +0800

    enhance debugging

--- html/routes/api.php
+++ html/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Plugin\DB;
 use eru123\router\Router;
 use Throwable;
 
@@ -9,14 +10,44 @@ $api->base('/api');
 $api->response(function ($data) {
     if (is_array($data) xor is_object($data)) {
         headers_sent() or header('Content-Type: application/json');
+        $data = (array) $data;
+        $res = [];
+
+        if (env('APP_ENV') === 'development') {
+            if (!isset($data['debug'])) {
+                $data['debug'] = [];
+            }
+
+            $data['debug']['response_debug'] = true;
+            $data['debug']['db_query'] = DB::instance()->queryHistory();
+            $data['debug']['memory'] = [
+                'usage' => ceil(memory_get_usage() / 1024 / 1024) . 'MB',
+                'usage_alloc' => ceil(memory_get_usage(true) / 1024 / 1024) . 'MB',
+                'peak' => ceil(memory_get_peak_usage() / 1024 / 1024) . 'MB',
+                'peak_alloc' => ceil(memory_get_peak_usage(true) / 1024 / 1024) . 'MB',
+            ];
+        } else {
+            unset($data['debug']);
+        }
+
         try {
-            echo json_encode($data, 0, JSON_ERROR_DEPTH | JSON_THROW_ON_ERROR);
+            $res = json_encode($data, 0, JSON_ERROR_DEPTH | JSON_THROW_ON_ERROR);
         } catch (Throwable $e) {
-            echo json_encode([
+            $res = json_encode([
                 'error' => $e->getMessage(),
                 'code' => $e->getCode(),
+            ], 0, JSON_ERROR_DEPTH | JSON_THROW_ON_ERROR);
+        }
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            http_response_code(500);
+            $res = json_encode([
+                'error' => json_last_error_msg(),
+                'code' => json_last_error(),
             ], 0);
         }
+
+        echo $res;
         exit;
     }
 
@@ -33,11 +64,23 @@ $api->fallback(function () {
 });
 
 $api->error(function (Throwable $e) {
-    http_response_code($e->getCode());
-    return [
+    http_response_code((int) $e->getCode());
+    $res = [
         'error' => $e->getMessage(),
         'code' => $e->getCode(),
     ];
+
+    if (env('APP_ENV') === 'development') {
+        $res['debug']['error'] = [
+            'message' => $e->getMessage(),
+            'code' => $e->getCode(),
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+            'trace' => $e->getTrace(),
+        ];
+    }
+
+    return $res;
 });
 
 $v1 = require __ROUTES__ . '/api/v1.php';

30be94540e567b2112ea80ab8e09df2dee6fcdd8
eru123 <yeoligoakino@gmail.com>
2023-07-27 17:59:05 +0800

    test daemon functions

--- html/daemon.php
+++ html/daemon.php
@@ -17,5 +17,30 @@ $d->run([
             $cyc = $c->cycle();
             echo "[{$date}]\tcycle: {$cyc}\tmem: {$mem}/{$mac}MB\n";
         }
+    },
+    function (Daemon $c) {
+        if ($c->is_second(0)) {
+            echo "second 0\n";
+        }
+    },
+    function (Daemon $c) {
+        if ($c->is_second(15)) {
+            echo "second 15\n";
+        }
+    },
+    function (Daemon $c) {
+        if ($c->is_second(30)) {
+            echo "second 30\n";
+        }
+    },
+    function (Daemon $c) {
+        if ($c->is_minute(0)) {
+            echo "minute 0\n";
+        }
+    },
+    function (Daemon $c) {
+        if ($c->is_minute(18)) {
+            echo "minute 18\n";
+        }
     }
 ]);

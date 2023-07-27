fe9ca12b6642530b23fb2338697bd73afb547670
eru123 <yeoligoakino@gmail.com>
2023-07-27 17:58:48 +0800

    added mail template routes

--- html/routes/api/v1.php
+++ html/routes/api/v1.php
@@ -18,5 +18,18 @@ $mail->post('/create', 'App\Controller\Mail@send');
 // $mail->post('/delete/$id', 'App\Controller\Mail@delete');
 // $mail->post('/list', 'App\Controller\Mail@list');
 
+$mailTemplate = new Router();
+$mailTemplate->base('/template');
+$mailTemplate->post('/create', 'App\Controller\MailTemplate@create');
+$mailTemplate->post('/id/$id/view', 'App\Controller\MailTemplate@view');
+$mailTemplate->post('/id/$id/update', 'App\Controller\MailTemplate@update');
+$mailTemplate->post('/id/$id/delete', 'App\Controller\MailTemplate@delete');
+$mailTemplate->post('/id/$id/trash', 'App\Controller\MailTemplate@view_deleted');
+$mailTemplate->post('/id/$id/restore', 'App\Controller\MailTemplate@restore');
+$mailTemplate->post('/code/$code/view', 'App\Controller\MailTemplate@view');
+$mailTemplate->post('/code/$code/update', 'App\Controller\MailTemplate@update');
+$mailTemplate->post('/code/$code/delete', 'App\Controller\MailTemplate@delete');
+$mail->child($mailTemplate);
+
 $v1->child($mail);
 return $v1;

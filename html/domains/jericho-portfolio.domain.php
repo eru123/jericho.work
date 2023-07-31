249f0090f0ee868480dbe3f002f517897e1c8882
eru123 <yeoligoakino@gmail.com>
2023-07-31 12:49:46 +0800

    added jericho portfolio domain

--- html/domains/jericho-portfolio.domain.php
+++ html/domains/jericho-portfolio.domain.php
@@ -7,4 +7,9 @@ $portfolio->get('/', function () {
     Router::status_page(200, 'Welcome!', 'Hi there! This is jericho-portfolio, a portfolio website for Jericho. <br /> Currently, this website is under construction, please come back later.');
 });
 
+$public_dir = realpath(__CWD__ . '/public');
+if ($public_dir && is_dir($public_dir)) {
+    $portfolio->static('/', [$public_dir], ['index.php', 'index.html']);
+}
+
 return $portfolio;

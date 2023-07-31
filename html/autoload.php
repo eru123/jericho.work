9b3fc4fbe19baa8128f0819f261338ee01f920fc
eru123 <yeoligoakino@gmail.com>
2023-07-31 12:50:27 +0800

    added logs path

--- html/autoload.php
+++ html/autoload.php
@@ -6,6 +6,7 @@ define('__APP__', __DIR__ . '/App');
 define('__SCRIPTS__', __DIR__ . '/scripts');
 define('__DOMAINS__', __DIR__ . '/domains');
 define('__ROUTES__', __DIR__ . '/routes');
+define('__LOGS__', __DIR__ . '/logs');
 
 require_once __DIR__ . '/vendor/autoload.php';
 eru123\config\DotEnv::load(__CWD__);

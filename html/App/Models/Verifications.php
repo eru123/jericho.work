a4afbba949244622b8416207927d6e22b3725d4f
eru123 <yeoligoakino@gmail.com>
2023-08-01 23:29:38 +0800

    Added verifications model

---
+++ html/App/Models/Verifications.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Models;
+
+use App\Plugin\DB;
+use eru123\orm\Raw;
+use PDOStatement;
+
+class Verifications implements Model
+{
+    public static function sanitize(array $data): array
+    {
+        $allowed = [
+            'user_id',
+            'type',
+            'identifier',
+            'code',
+            'action',
+            'status',
+            'expires_at',
+            'created_at',
+            'updated_at',
+            'deleted_at',
+        ];
+
+        $sanitized = [];
+        foreach ($allowed as $key) {
+            if (isset($data[$key])) {
+                $sanitized[$key] = $data[$key];
+            }
+        }
+
+        return $sanitized;
+    }
+
+    public static function insert(array $data): PDOStatement
+    {
+        $data = static::sanitize($data);
+        $date = date('Y-m-d H:i:s');
+        $data['created_at'] = $date;
+        $data['updated_at'] = $date;
+        return DB::instance()->insert('verifications', $data);
+    }
+
+    public static function insert_many(array $data): PDOStatement
+    {
+        $sanitized = [];
+        $date = date('Y-m-d H:i:s');
+        foreach ($data as $row) {
+            $tmp = static::sanitize($row);
+            $tmp['created_at'] = $date;
+            $tmp['updated_at'] = $date;
+            $sanitized[] = $tmp;
+        }
+
+        return DB::instance()->insert_many('verifications', $sanitized);
+    }
+
+    public static function update(int|string $id, array $data): PDOStatement
+    {
+        $data = static::sanitize($data);
+        $data['updated_at'] = date('Y-m-d H:i:s');
+
+
+        if (is_numeric($id)) {
+            return DB::instance()->update('verifications', $data, ['id' => $id]);
+        }
+
+        return DB::instance()->update('verifications', $data, $id);
+    }
+
+    public static function delete(int|string $id): PDOStatement
+    {
+        if (is_numeric($id)) {
+            return DB::instance()->update('verifications', ['deleted_at' => date('Y-m-d H:i:s')], ['id' => $id]);
+        }
+
+        return DB::instance()->update('verifications', ['deleted_at' => date('Y-m-d H:i:s')], $id);
+    }
+
+    public static function deleteUnsafe(int|string $id): PDOStatement
+    {
+        if (is_numeric($id)) {
+            return DB::instance()->delete('verifications', ['id' => $id]);
+        }
+
+        return DB::instance()->delete('verifications', $id);
+    }
+
+    public static function purge(int|string $id = null): PDOStatement
+    {
+        if (is_null($id)) {
+            return DB::instance()->delete('verifications', '`deleted_at` IS NOT NULL');
+        }
+
+        if (is_numeric($id)) {
+            return DB::instance()->delete('verifications', (string) Raw::build('id = ? AND deleted_at IS NOT NULL', [$id]));
+        }
+
+        return DB::instance()->delete('verifications', $id);
+    }
+
+    public static function find(int|string $id): array|null
+    {
+        if (is_numeric($id)) {
+            return DB::instance()->query('SELECT * FROM `verifications` WHERE `id` = ? AND `deleted_at` IS NULL', [$id])->fetch();
+        }
+
+        return DB::instance()->query('SELECT * FROM `verifications` WHERE `deleted_at` IS NULL AND ' . $id)->fetch();
+    }
+
+    public static function find_many(int|string $id): array
+    {
+        if (is_numeric($id)) {
+            return DB::instance()->query('SELECT * FROM `verifications` WHERE `id` = ? AND `deleted_at` IS NULL', [$id])->fetchAll();
+        }
+
+        return DB::instance()->query('SELECT * FROM `verifications` WHERE `deleted_at` IS NULL AND ' . $id)->fetchAll();
+    }
+}

147b0aac27abe1a7b939dc4654abc0482493ee85
eru123 <yeoligoakino@gmail.com>
2023-08-02 15:32:58 +0800

    added main client

--- html/gulpfile.js
+++ html/gulpfile.js
@@ -11,6 +11,10 @@ gulp.task('admin-install', function (done) {
   spawn('pnpm', ['install'], { cwd: './client/admin' }).on('close', done);
 });
 
+gulp.task('main-install', function (done) {
+  spawn('pnpm', ['install'], { cwd: './client/main' }).on('close', done);
+});
+
 
 // Update
 
@@ -20,6 +24,10 @@ gulp.task('cdn-update', function (done) {
 
 gulp.task('admin-update', function (done) {
   spawn('pnpm', ['update'], { cwd: './client/admin' }).on('close', done);
+});
+
+gulp.task('main-update', function (done) {
+  spawn('pnpm', ['update'], { cwd: './client/main' }).on('close', done);
 });
 
 // Dev
@@ -32,6 +40,10 @@ gulp.task('admin-dev', function (done) {
   spawn('pnpm', ['run', 'dev'], { cwd: './client/admin' }).on('close', done);
 });
 
+gulp.task('main-dev', function (done) {
+  spawn('pnpm', ['run', 'dev'], { cwd: './client/main' }).on('close', done);
+});
+
 // Build
 
 gulp.task('cdn-build', function (done) {
@@ -42,13 +54,18 @@ gulp.task('admin-build', function (done) {
   spawn('pnpm', ['run', 'build'], { cwd: './client/admin' }).on('close', done);
 });
 
+gulp.task('main-build', function (done) {
+  spawn('pnpm', ['run', 'build'], { cwd: './client/main' }).on('close', done);
+});
+
+
 // CF Cache Purge
 
 gulp.task('cf_cache_purge', function (done) {
   spawn('/bin/php', ['cf_cache_purge.php'], { cwd: './scripts' }).on('close', done);
 });
 
-gulp.task('install', gulp.parallel('cdn-install', 'admin-install'));
-gulp.task('update', gulp.parallel('cdn-update', 'admin-update'));
-gulp.task('dev', gulp.parallel('cdn-dev', 'admin-dev'));
-gulp.task('build', gulp.series(gulp.parallel('cdn-build', 'admin-build'), 'cf_cache_purge'));
+gulp.task('install', gulp.parallel('cdn-install', 'admin-install', 'main-install'));
+gulp.task('update', gulp.parallel('cdn-update', 'admin-update', 'main-update'));
+gulp.task('dev', gulp.parallel('cdn-dev', 'admin-dev', 'main-dev'));
+gulp.task('build', gulp.series(gulp.parallel('cdn-build', 'admin-build', 'main-build'), 'cf_cache_purge'));

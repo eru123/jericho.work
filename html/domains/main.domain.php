8975df9ddd284bdfe6113aced21129f0d7ebfed1
eru123 <yeoligoakino@gmail.com>
2023-08-02 15:33:19 +0800

    added main client

--- html/domains/main.domain.php
+++ html/domains/main.domain.php
@@ -13,21 +13,24 @@ $alternative_links = [
     'OpenCDN' => '/cdn',
 ];
 
-$router->get('/', function () use ($alternative_links) {
-    $msg = 'Hi there! Thanks for visiting,<br /> the website is currently under construction, please come back later. <br /><br />';
-    $msg .= 'In the meantime, you can visit the our other services that are working: ' . implode(', ', array_map(function ($name, $link) {
-        $name = htmlspecialchars($name);
-        $link = htmlspecialchars($link);
-        return "<a href=\"$link\">$name</a>";
-    }, array_keys($alternative_links), $alternative_links));
-    Router::status_page(200, 'Welcome!', $msg);
-});
+// $router->get('/', function () use ($alternative_links) {
+//     $msg = 'Hi there! Thanks for visiting,<br /> the website is currently under construction, please come back later. <br /><br />';
+//     $msg .= 'In the meantime, you can visit the our other services that are working: ' . implode(', ', array_map(function ($name, $link) {
+//         $name = htmlspecialchars($name);
+//         $link = htmlspecialchars($link);
+//         return "<a href=\"$link\">$name</a>";
+//     }, array_keys($alternative_links), $alternative_links));
+//     Router::status_page(200, 'Welcome!', $msg);
+// });
+
+$router->get('/', 'App\Controller\Main@index');
 
 $public_dir = realpath(__CWD__ . '/public');
 if ($public_dir && is_dir($public_dir)) {
     $router->static('/', [$public_dir], ['index.php', 'index.html']);
 }
 
+$router->static('/', [__CLIENT__ . '/main/dist'], [], 'App\Controller\Main@index');
 
 $cdn = require __ROUTES__  . '/cdn.php';
 $admin = require __ROUTES__  . '/admin.php';

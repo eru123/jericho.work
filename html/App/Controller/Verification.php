70c6f9ac5415b887ea8e461a6580c4dae2363240
eru123 <yeoligoakino@gmail.com>
2023-08-02 15:35:26 +0800

    init verfication

---
+++ html/App/Controller/Verification.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Plugin\DB;
+use eru123\orm\Raw;
+use PDOStatement;
+use App\Models\Verifications;
+
+class Verification
+{
+    public static function generate_code(int $len = 6): string
+    {
+        return substr(str_shuffle(str_repeat('0123456789', $len)), 0, $len);
+    }
+
+    public static function add_mail(string $user_id, string $email)
+    {
+        $code = static::generate_code(6);
+        $data = [
+            'user_id' => $user_id,
+            'type' => 'email',
+            'identifier' => $email,
+            'code' => $code,
+            'action' => 'add_mail',
+            'status' => false,
+            'expires_at' => date('Y-m-d H:i:s', strtotime('+1 day')),
+        ];
+        Verifications::insert($data);
+        return $code;
+    }
+}

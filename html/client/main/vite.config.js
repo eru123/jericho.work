32f2e2c8a25a02731c050f3f81a1a92a20e3a4d8
eru123 <yeoligoakino@gmail.com>
2023-08-02 15:36:50 +0800

    init main client

---
+++ html/client/main/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  publicDir: 'public',
+  base: '',
+  build: {
+    manifest: true,
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsDir: '__',
+    rollupOptions: {
+      input: {
+        main: 'src/main.js'
+      }
+    }
+  },
+})

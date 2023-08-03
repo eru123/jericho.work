9c681b293ff1ec78bce78a2b38eca4d4dc9a3fd0
eru123 <yeoligoakino@gmail.com>
2023-08-03 15:29:41 +0800

    separate viewer to index

--- html/App/Controller/Main.php
+++ html/App/Controller/Main.php
@@ -7,6 +7,22 @@ use eru123\router\Context;
 
 class Main extends Controller
 {
+    public function view()
+    {
+        $vite = Vite::instance();
+        $vite->setDist(__CLIENT__ . '/main/dist');
+        $vite->setAppId('app');
+        $vite->useTemplate('vite');
+        $vite->header('<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">');
+        $vite->header('<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">');
+        $vite->header('<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">');
+        $vite->header('<link rel="manifest" href="/site.webmanifest">');
+        $vite->header('<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">');
+        $vite->header('<meta name="msapplication-TileColor" content="#2b5797">');
+        $vite->header('<meta name="theme-color" content="#ffffff">');
+        return $vite->render();
+    }
+
     public function index(Context $c)
     {
         if ($c->file_path) {
@@ -14,13 +30,10 @@ class Main extends Controller
         }
 
         $vite = Vite::instance();
-        $vite->setDist(__CLIENT__ . '/main/dist');
-        $vite->setAppId('app');
-        $vite->useTemplate('vite');
         $vite->data([
             'app_title' => env('APP_TITLE', 'App'),
         ]);
-        $vite->header("<link rel=\"icon\" href=\"/favicon.ico\">");
-        return $vite->render();
+
+        return $this->view();
     }
 }

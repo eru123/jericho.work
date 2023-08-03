8b3338479b3d63affd81903ed9ce105fb9d86477
eru123 <yeoligoakino@gmail.com>
2023-08-03 18:01:05 +0800

    extend tailwind theme

--- html/client/main/tailwind.config.js
+++ html/client/main/tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
     content: [
         "./src/**/*.{vue,scss}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'primary-900': '#00222b',
+                'primary-800': '#004455',
+                'primary-700': '#006680',
+                'primary-600': '#0088aa',
+                'primary-500': '#00aad4',
+                'primary-400': '#33aacf',
+                'primary-300': '#2ad4ff',
+                'primary-200': '#55ddff',
+                'primary-100': '#aaeeff',
+                'primary-50': '#d5f6ff',
+            }
+        }
     },
     plugins: [],
 }

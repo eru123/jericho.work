27ba5b0977583a56fbaad0d2fb40c3642ff22b9a
eru123 <yeoligoakino@gmail.com>
2023-08-03 18:01:44 +0800

    test extended tailwind colors

--- html/client/main/src/views/Landing.vue
+++ html/client/main/src/views/Landing.vue
@@ -4,7 +4,16 @@ import { ref } from 'vue'
 </script>
 <template>
    <div>
-      <h1>Landing Page</h1>
+      <h1 class="text-primary-900">Landing Page</h1>
+      <h1 class="text-primary-800">Landing Page</h1>
+      <h1 class="text-primary-700">Landing Page</h1>
+      <h1 class="text-primary-600">Landing Page</h1>
+      <h1 class="text-primary-500">Landing Page</h1>
+      <h1 class="text-primary-400">Landing Page</h1>
+      <h1 class="text-primary-300">Landing Page</h1>
+      <h1 class="text-primary-200">Landing Page</h1>
+      <h1 class="text-primary-100">Landing Page</h1>
+      <h1 class="text-primary-50">Landing Page</h1>
    </div>
 </template>
 <style scoped>

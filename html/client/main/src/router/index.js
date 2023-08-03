93ce42993b294452d762d83d7f345fd16cab84f8
eru123 <yeoligoakino@gmail.com>
2023-08-03 17:40:44 +0800

    added landing page

--- html/client/main/src/router/index.js
+++ html/client/main/src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Landing from '@/views/Landing.vue'
 
 const routes = [
+    {
+        path: '/',
+        name: 'Landing',
+        component: Landing
+    },
+];
 
-]
 
 const router = createRouter({
     history: createWebHistory(),

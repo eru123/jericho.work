1322599228700657c853f70f24f888a19cf16d7e
eru123 <yeoligoakino@gmail.com>
2023-08-03 15:30:34 +0800

    added webmanifest path

--- html/routes/main.php
+++ html/routes/main.php
@@ -5,5 +5,6 @@ use eru123\router\Router;
 return function (Router &$main) {
     $main->fallback('App\Controller\Main@index');
     $main->static('/', [__CLIENT__ . '/main/dist'], []);
+    $main->any('/site.webmanifest', 'App\Controller\Main@webmanifest');
     $main->get('/', 'App\Controller\Main@index');
 };

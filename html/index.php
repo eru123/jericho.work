4622dd50e3de7215d3a12027cb6f32cff8d5e1b6
eru123 <yeoligoakino@gmail.com>
2023-08-07 21:00:12 +0800

    implement default handler

--- html/index.php
+++ html/index.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/autoload.php';
 use App\Plugin\Domain;
 
 $domain = Domain::instance(__DOMAINS__);
+$domain->setDefaultHandler('main');
 $domain->createVirtualHost(env('DOMAIN'), 'main');
 $domain->createVirtualHost(env('CDN_DOMAIN'), 'main');
 $domain->createVirtualHost(env('ADMIN_DOMAIN'), 'main');

1085d39007b4accd09c8ef7920a5edacc07a0e45
eru123 <yeoligoakino@gmail.com>
2023-08-09 16:22:22 +0800

    update

--- docker_dev.sh
+++ docker_dev.sh
@@ -1,7 +1,12 @@
 sudo docker stop dev
 sudo docker rm dev
 sudo docker build -t dev:1 .
-sudo docker run -d -p 8080:80 --name dev dev:1
+sudo docker run -d -p 8080:80 -p 3000:3000 --name dev \
+    -v /var/www/jericho.work/html:/app \
+    dev:1 
 
 # enter container
-sudo docker exec -it dev sh
+# sudo docker exec -it dev sh
+
+# logs container
+sudo docker logs -f dev

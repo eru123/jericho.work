3bc836ceaec2da3d625012e7aa795aa132f87729
eru123 <yeoligoakino@gmail.com>
2023-08-13 23:59:51 +0800

    server debug data console logging

--- html/client/main/src/App.vue
+++ html/client/main/src/App.vue
@@ -1,3 +1,15 @@
+<script setup>
+import { watchEffect } from "vue";
+import useServerData from "@/composables/useServerData";
+const $server = useServerData();
+
+watchEffect(() => {
+    if ($server?.debug) {
+        console.log($server);
+    }
+});
+
+</script>
 <template>
     <router-view />
 </template>

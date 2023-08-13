6272a7b678faf3e6c768fbe32347c9473ded0e81
eru123 <yeoligoakino@gmail.com>
2023-08-13 12:08:40 +0800

    Fix meta bug and duplication bug

--- html/scripts/mail_queue.php
+++ html/scripts/mail_queue.php
@@ -10,12 +10,28 @@ use App\Models\Smtps;
 $args = getopt('q:');
 
 if (isset($args['q']) && !empty($args['q'])) {
+    $mc = MC::instance();
     $id = $args['q'];
+    $proc = Mailer::CACHE_PREFIX . 'queue_process_' . $id;
+    if ($mc->get($proc)) {
+        echo "Mail queue {$id} is being processed.", PHP_EOL;
+        exit(1);
+    }
+
+    $mc->set($proc, 1, 60);
+
     $mail = Mails::find($id);
-    if (!$mail) {
+    if (!$mail || @$mail['status'] != Mails::STATUS_QUEUE) {
         echo "Mail queue not found.", PHP_EOL;
         exit(1);
     }
+
+    $mail['to'] = is_string($mail['to']) ? json_decode($mail['to'], true) : $mail['to'] ?? [];
+    $mail['cc'] = is_string($mail['cc']) ? json_decode($mail['cc'], true) : $mail['cc'] ?? [];
+    $mail['bcc'] = is_string($mail['bcc']) ? json_decode($mail['bcc'], true) : $mail['bcc'] ?? [];
+    $mail['meta'] = is_string($mail['meta']) ? json_decode($mail['meta'], true) : $mail['meta'] ?? [];
+    $mail['response'] = is_string($mail['response']) ? json_decode($mail['response'], true) : $mail['response'] ?? [];
+    $mail['attachments'] = is_string($mail['attachments']) ? json_decode($mail['attachments'], true) : $mail['attachments'] ?? [];
 
     $mail_identifier = Mailer::CACHE_PREFIX . '0_' . time();
     $limit = env('SMTP_LIMIT_PER_SECOND', 14);
@@ -33,7 +49,6 @@ if (isset($args['q']) && !empty($args['q'])) {
         $limit = $smtp['limit'];
     }
 
-    $mc = MC::instance();
     $ctr = intval($mc->get($mail_identifier));
     if ($ctr >= $limit) {
         echo "SMTP limit reached.", PHP_EOL;
@@ -50,7 +65,7 @@ if (isset($args['q']) && !empty($args['q'])) {
             'response' => $newdata['response']
         ]);
     } catch (Exception $e) {
-        $requeue = static::queue([
+        $requeue = Mailer::queue([
             'parent_id' => $mail['id'],
             'sender_id' => $mail['sender_id'],
             'user_id' => $mail['user_id'],

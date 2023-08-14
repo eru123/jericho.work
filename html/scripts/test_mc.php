908a299d49667f86e3a41b63c68eb98248483f79
eru123 <yeoligoakino@gmail.com>
2023-08-14 11:39:02 +0800

    fix MC plugin test

--- html/scripts/test_mc.php
+++ html/scripts/test_mc.php
@@ -3,10 +3,27 @@ require_once __DIR__ . '/autoload.php';
 
 use App\Plugin\MC;
 
-$mc = MC::instance();
-for ($i = 0; $i < 10; $i++) {
-    echo "Last OTP Code: " . $mc->get('otp:code') . PHP_EOL;
-    $code = rand(100000, 999999);
-    echo "OTP Code: {$code}\n";
-    $mc->set("otp:code", $code, 60);
+$loaded = extension_loaded('memcached');
+echo "[Test] " . ($loaded ? 'OK Memcached extension loaded' : 'FAILED Memcached extension not loaded') . PHP_EOL;
+
+if (!$loaded) {
+    exit;
 }
+
+$mc = MC::instance();
+$test_count = 10;
+$last_code = null;
+$passed = 0;
+$identifier = 'test_mc:code';
+for ($i = 0; $i < $test_count + 1; $i++) {
+    if ($i != 0 && $last_code == $mc->get($identifier) && $last_code) {
+        $passed++;
+        if ($i == $test_count) break;
+    }
+
+    $code = rand(100000, 999999);
+    $mc->set($identifier, $code, 1);
+    $last_code = $code;
+}
+
+echo "[Test] OK Memcached passed $passed/$test_count tests", PHP_EOL;

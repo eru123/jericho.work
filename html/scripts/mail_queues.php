bdb05763afe1b7557a64398d8d0120fe57348a99
eru123 <yeoligoakino@gmail.com>
2023-08-14 14:09:51 +0800

    fix missing date var

--- html/scripts/mail_queues.php
+++ html/scripts/mail_queues.php
@@ -6,6 +6,7 @@ use App\Plugin\Mailer;
 
 try {
     $processed = mailer::send_queues();
+    $date = date('Y-m-d H:i:s');
     writelog("[{$date}]\tProcessed Mail Queues: {$processed}");
 } catch (Exception $e) {
     throw $e;

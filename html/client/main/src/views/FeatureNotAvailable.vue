02d6fa270839503562651ab144ebcfffd0882e5e
eru123 <yeoligoakino@gmail.com>
2023-08-17 17:58:30 +0800

    added feature not available component

---
+++ html/client/main/src/views/FeatureNotAvailable.vue
@@ -0,0 +1,45 @@
+<script setup>
+import PublicPage from "@/components/PublicPage.vue";
+</script>
+<template>
+    <PublicPage>
+        <div class="container">
+            <v-icon class="icon" name="md-appsoutage-twotone" />
+            <h1>Feature Not Available</h1>
+            <p>The page you are trying to access is currently not available.</p>
+        </div>
+    </PublicPage>
+</template>
+<style scoped lang="scss">
+.container {
+    @apply w-full max-w-screen-md mx-auto min-h-[calc(100vh-3.5rem)] flex flex-col justify-center items-center text-center px-4 py-8;
+
+    .icon {
+        @apply mb-6 h-[4rem] w-[4rem];
+
+        &.success {
+            @apply text-green-600;
+        }
+
+        &.error {
+            @apply text-red-500;
+        }
+    }
+
+    h1 {
+        @apply text-2xl font-semibold text-primary-900 mb-2;
+
+        span {
+            @apply mt-2 block uppercase text-primary-800 text-4xl font-bold;
+        }
+    }
+
+    p {
+        @apply text-lg text-primary-900 mx-auto max-w-prose mb-4;
+    }
+
+    a {
+        @apply px-4 py-2 rounded-md text-sm font-normal text-primary-50 bg-primary-900 hover:bg-primary-800;
+    }
+}
+</style>

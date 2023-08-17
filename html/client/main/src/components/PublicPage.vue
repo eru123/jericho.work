a7cf294215c747be40888b6eb9e3556e34bc3ea9
eru123 <yeoligoakino@gmail.com>
2023-08-17 16:44:44 +0800

    added public page component

---
+++ html/client/main/src/components/PublicPage.vue
@@ -0,0 +1,16 @@
+<script setup>
+import PublicFooter from "@/components/PublicFooter.vue";
+import PublicHeader from "@/components/PublicHeader.vue";
+</script>
+<template>
+    <PublicHeader />
+    <div class="public-page-container">
+        <slot></slot>
+    </div>
+    <PublicFooter />
+</template>
+<style scoped lang="scss">
+.public-page-container {
+    @apply w-full min-h-[calc(100vh-3.5rem)] max-w-full;
+}
+</style>

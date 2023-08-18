c0dfc4dca777183f8e77aa2b2752f58f5f25e920
eru123 <yeoligoakino@gmail.com>
2023-08-19 03:46:20 +0800

    update main route

--- html/routes/main.php
+++ html/routes/main.php
@@ -1,16 +1,36 @@
 <?php
 
 use eru123\router\Router;
+use eru123\router\ViteExeption;
 use App\Plugin\Vite;
+use App\Controller\Main;
 
 return function (Router &$main) {
     Vite::src(__CLIENT__ . '/main/src');
     Vite::public(__CLIENT__ . '/main/public');
     Vite::dist(__CLIENT__ . '/main/dist');
     Vite::manifest(__CLIENT__ . '/main/dist/manifest.json');
-    Vite::template(env('APP_ENV', 'production') === 'production' ? 'vite' : 'dev');
-    Vite::set('base_uri', env('APP_ENV', 'production') === 'production' ? '' : 'http://localhost:3000');
+    Vite::template(env('MAIN_VITE_TEMPLATE', 'vite'));
+    Vite::set('base_uri', env('MAIN_VITE_BASE_URI', ''));
     Vite::inject($main);
+
+    $main->error(function (Throwable $r) {
+        if ($r instanceof ViteExeption) {
+            return Router::status_page(500, '500 Internal Server Error', 'Vite: '. trim($r->getMessage(), '"'));
+        }
+
+        Vite::data([
+            'error' => [
+                'code' => $r->getCode(),
+                'message' => $r->getMessage(),
+                'file' => $r->getFile(),
+                'line' => $r->getLine(),
+                'trace' => $r->getTrace(),
+            ]
+        ]);
+
+        return (new Main)->view();
+    });
 
     $main->fallback('App\Controller\Main@index');
     $main->any('/site.webmanifest', 'App\Controller\Main@webmanifest');

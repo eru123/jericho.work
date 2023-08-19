5b309bdcbcb76fc9955b85b64ca3bdbdb1fffbae
eru123 <yeoligoakino@gmail.com>
2023-08-19 13:02:06 +0800

    added server error page

--- html/client/main/src/App.vue
+++ html/client/main/src/App.vue
@@ -1,15 +1,20 @@
 <script setup>
 import { watchEffect } from "vue";
 import useServerData from "@/composables/useServerData";
+import ServerError from "@/views/ServerError.vue";
+
 const $server = useServerData();
 
 watchEffect(() => {
     if ($server?.debug) {
-        console.log($server);
+        console.log("Server Data", $server);
+        if ($server?.error) {
+            console.log("Server error: ", $server?.error);
+        }
     }
 });
-
 </script>
 <template>
-    <router-view />
+    <ServerError v-if="$server?.error" />
+    <router-view v-else />
 </template>

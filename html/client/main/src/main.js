498a2f8b77b6a32fadcad936cb0759775c0ed655
eru123 <yeoligoakino@gmail.com>
2023-08-19 23:47:22 +0800

    added icons for dialogues

--- html/client/main/src/main.js
+++ html/client/main/src/main.js
@@ -1,9 +1,9 @@
-import { createApp } from 'vue'
-import useServerData from './composables/useServerData'
-import router from './router'
-import './style.scss'
-import App from './App.vue'
-import Link from './components/Link.vue'
+import { createApp } from "vue";
+import useServerData from "./composables/useServerData";
+import router from "./router";
+import "./style.scss";
+import App from "./App.vue";
+import Link from "./components/Link.vue";
 import { OhVueIcon, addIcons } from "oh-vue-icons";
 import {
     BiDiscord,
@@ -19,7 +19,11 @@ import {
     BiXCircle,
     CoLinkBroken,
     MdAppsoutageTwotone,
-    IoLogoAppleAr 
+    IoLogoAppleAr,
+    BiInfoCircle,
+    MdDangerousOutlined,
+    CoWarning,
+    BiQuestionCircle,
 } from "oh-vue-icons/icons";
 
 addIcons(
@@ -36,12 +40,16 @@ addIcons(
     BiXCircle,
     CoLinkBroken,
     MdAppsoutageTwotone,
-    IoLogoAppleAr 
+    IoLogoAppleAr,
+    BiInfoCircle,
+    MdDangerousOutlined,
+    CoWarning,
+    BiQuestionCircle
 );
 
-const app = createApp(App)
+const app = createApp(App);
 app.component("v-icon", OhVueIcon);
-app.component('v-link', Link)
-app.provide('$server', useServerData())
-app.use(router)
-app.mount('#app')
+app.component("v-link", Link);
+app.provide("$server", useServerData());
+app.use(router);
+app.mount("#app");

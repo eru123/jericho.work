0f26a72a4ca441c7118f70f80babff3257d16076
eru123 <yeoligoakino@gmail.com>
2023-08-21 00:07:56 +0800

    update dialogues component

--- html/client/main/src/components/Dialogs.vue
+++ html/client/main/src/components/Dialogs.vue
@@ -11,28 +11,65 @@ const dialogs = useDialogs();
                 class="dialog-container"
             >
                 <div v-if="dialog.type === 'info'" class="dialog info">
-                    <div class="title"><v-icon class="icon info" name="bi-info-circle"/>{{ dialog.title }}</div>
+                    <div class="title">
+                        <v-icon class="icon info" name="bi-info-circle" />{{
+                            dialog.title
+                        }}
+                    </div>
                     <div class="message">{{ dialog.message }}</div>
                     <div class="actions right">
-                        <button class="primary" @click="typeof dialog?.onOk === 'function' ? dialog.onOk(dialog.close) : dialog.close()">
+                        <button
+                            class="primary"
+                            @click="
+                                typeof dialog?.onOk === 'function'
+                                    ? dialog.onOk(dialog.close)
+                                    : dialog.close()
+                            "
+                        >
                             OK
                         </button>
                     </div>
                 </div>
-                <div v-else-if="dialog.type === 'warning'" class="dialog warning">
-                    <div class="title"><v-icon class="icon warning" name="co-warning"/>{{ dialog.title }}</div>
+                <div
+                    v-else-if="dialog.type === 'warning'"
+                    class="dialog warning"
+                >
+                    <div class="title">
+                        <v-icon class="icon warning" name="co-warning" />{{
+                            dialog.title
+                        }}
+                    </div>
                     <div class="message">{{ dialog.message }}</div>
                     <div class="actions right">
-                        <button class="primary" @click="typeof dialog?.onOk === 'function' ? dialog.onOk(dialog.close) : dialog.close()">
+                        <button
+                            class="primary"
+                            @click="
+                                typeof dialog?.onOk === 'function'
+                                    ? dialog.onOk(dialog.close)
+                                    : dialog.close()
+                            "
+                        >
                             OK
                         </button>
                     </div>
                 </div>
                 <div v-else-if="dialog.type === 'error'" class="dialog danger">
-                    <div class="title"><v-icon class="icon danger" name="md-dangerous-outlined"/>{{ dialog.title }}</div>
+                    <div class="title">
+                        <v-icon
+                            class="icon danger"
+                            name="md-dangerous-outlined"
+                        />{{ dialog.title }}
+                    </div>
                     <div class="message">{{ dialog.message }}</div>
                     <div class="actions right">
-                        <button class="primary" @click="typeof dialog?.onOk === 'function' ? dialog.onOk(dialog.close) : dialog.close()">
+                        <button
+                            class="primary"
+                            @click="
+                                typeof dialog?.onOk === 'function'
+                                    ? dialog.onOk(dialog.close)
+                                    : dialog.close()
+                            "
+                        >
                             OK
                         </button>
                     </div>
@@ -41,19 +78,64 @@ const dialogs = useDialogs();
                     v-else-if="dialog.type === 'confirm'"
                     class="dialog confirm"
                 >
-                    <div class="title"><v-icon class="icon confirm" name="bi-question-circle"/>{{ dialog.title }}</div>
+                    <div class="title">
+                        <v-icon
+                            class="icon confirm"
+                            name="bi-question-circle"
+                        />{{ dialog.title }}
+                    </div>
                     <div class="message">{{ dialog.message }}</div>
                     <div class="actions right">
                         <button
                             class="secondary"
-                            @click="typeof dialog?.onCancel === 'function' ? dialog.onCancel(dialog.close) : dialog.close()"
+                            @click="
+                                typeof dialog?.onCancel === 'function'
+                                    ? dialog.onCancel(dialog.close)
+                                    : dialog.close()
+                            "
                         >
                             Cancel
                         </button>
-                        <button class="primary" @click="typeof dialog?.onOk === 'function' ? dialog.onOk(dialog.close) : dialog.close()">
+                        <button
+                            class="primary"
+                            @click="
+                                typeof dialog?.onOk === 'function'
+                                    ? dialog.onOk(dialog.close)
+                                    : dialog.close()
+                            "
+                        >
                             OK
                         </button>
                     </div>
+                </div>
+                <div
+                    v-else-if="dialog.type === 'loading' && !dialog?.message"
+                    class="dialog loading no-message"
+                >
+                    <v-icon
+                        class="icon loading loading-1"
+                        name="bi-hourglass-split"
+                    />
+                    <v-icon
+                        class="icon loading loading-2"
+                        name="bi-hourglass-bottom"
+                    />
+                </div>
+                <div
+                    v-else-if="dialog.type === 'loading' && dialog?.message"
+                    class="dialog loading with-message"
+                >
+                    <div class="icon-container">
+                        <v-icon
+                            class="icon loading loading-1"
+                            name="bi-hourglass-split"
+                        />
+                        <v-icon
+                            class="icon loading loading-2"
+                            name="bi-hourglass-bottom"
+                        />
+                    </div>
+                    <div class="message">{{ dialog.message }}</div>
                 </div>
             </div>
         </div>
@@ -94,6 +176,78 @@ const dialogs = useDialogs();
 
                     &.warning {
                         @apply text-orange-700;
+                    }
+                }
+            }
+
+            &.loading {
+                @apply w-32 h-32;
+
+                .icon.loading {
+                    @apply text-primary-700 w-[3rem] h-[3rem] min-w-[3rem] min-h-[3rem];
+                    position: absolute;
+                    opacity: 0;
+
+                    &.loading-1 {
+                        animation: loading-1 1s infinite ease-in-out;
+                    }
+
+                    &.loading-2 {
+                        animation: loading-2 1s infinite ease-in-out;
+                    }
+                }
+
+                &.no-message {
+                    @apply rounded-full w-[4rem] h-[4rem] min-w-[4rem] min-h-[4rem] p-2;
+                }
+
+                &.with-message {
+                    @apply flex flex-col items-center justify-center min-w-[200px] px-2 py-4 h-fit;
+
+                    .icon-container {
+                        @apply flex flex-col items-center justify-center;
+                        @apply w-full h-[3rem];
+                    }
+
+                    .message {
+                        @apply w-full block text-center text-base text-gray-700;
+                    }
+                }
+
+                @keyframes loading-1 {
+                    0% {
+                        opacity: 1;
+                    }
+                    50% {
+                        opacity: 1;
+                    }
+                    51% {
+                        opacity: 0;
+                    }
+                    100% {
+                        opacity: 0;
+                    }
+                }
+
+                @keyframes loading-2 {
+                    0% {
+                        opacity: 0;
+                    }
+                    50% {
+                        opacity: 0;
+                    }
+                    51% {
+                        opacity: 1;
+                    }
+                    70% {
+                        transform: rotate(0deg);
+                    }
+                    80% {
+                        transform: rotate(180deg);
+                    }
+                    100% {
+                        transform: rotate(180deg);
+                        opacity: 1;
                     }
                 }
             }

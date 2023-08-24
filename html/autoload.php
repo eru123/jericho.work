7318f84d63fa4e630ce8d00089fde85c2169304f
eru123 <yeoligoakino@gmail.com>
2023-08-24 18:00:33 +0800

    added database paths

--- html/autoload.php
+++ html/autoload.php
@@ -7,6 +7,9 @@ define('__SCRIPTS__', __DIR__ . DIRECTORY_SEPARATOR . 'scripts');
 define('__DOMAINS__', __DIR__ . DIRECTORY_SEPARATOR . 'domains');
 define('__ROUTES__', __DIR__ . DIRECTORY_SEPARATOR . 'routes');
 define('__LOGS__', __DIR__ . DIRECTORY_SEPARATOR . 'logs');
+define('__DATABASE__', __DIR__ . DIRECTORY_SEPARATOR . 'database');
+define('__MIGRATIONS__', __DATABASE__ . DIRECTORY_SEPARATOR . 'migrations');
+define('__SEEDS__', __DATABASE__ . DIRECTORY_SEPARATOR . 'seeds');
 
 if (!class_exists('Memcached') && PHP_OS_FAMILY == 'Windows') {
     include("memcached.php");

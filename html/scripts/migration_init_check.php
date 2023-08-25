0accd978d85500810f46f50b95ff85735a2ab508
eru123 <yeoligoakino@gmail.com>
2023-08-25 09:09:27 +0800

    moving migrations to independent script

--- html/scripts/migration_init_check.php
+++ html/scripts/migration_init_check.php
@@ -66,62 +66,8 @@ try {
             exit(1);
         }
 
-        echo "[Migration] OK Executing migrations", PHP_EOL;
-        $files = glob(__MIGRATIONS__ . DIRECTORY_SEPARATOR . '*.sql');
-        $sanitized_files = [];
-
-        foreach ($files as $file) {
-            $file = realpath($file);
-            if (!$file || !is_file($file)) {
-                echo "[Migration] FAILED $file does not exist", PHP_EOL;
-            }
-            if (!is_readable($file)) {
-                echo "[Migration] FAILED $file is not readable", PHP_EOL;
-            }
-            $sanitized_files[] = $file;
-        }
-
-        $basenames = array_map(fn ($file) => basename($file), $sanitized_files);
-        $placeholders = array_map(fn () => '?', $basenames);
-
-        $sql = "SELECT * FROM `migrations` WHERE `name` NOT IN (" . implode(", ", $placeholders) . ")";
-        $stmt = $pdo->prepare("SELECT * FROM `migrations` WHERE `name` IN (" . implode(", ", $placeholders) . ")");
-        $stmt->execute($basenames);
-        $result = $stmt->fetchAll();
-
-        foreach ($result as $row) {
-            $key = array_search($row['name'], $basenames);
-            if ($key !== false) {
-                unset($sanitized_files[$key]);
-            }
-        }
-
-        usort($sanitized_files, function ($a, $b) {
-            $a = explode('_', basename($a));
-            $b = explode('_', basename($b));
-            $a = (int) $a[0];
-            $b = (int) $b[0];
-            return $a - $b;
-        });
-
-        try {
-            foreach ($sanitized_files as $file) {
-                $sql = file_get_contents($file);
-                $stmt = $pdo->prepare($sql);
-                $stmt->execute();
-                $stmt->closeCursor();
-
-                echo "[Migration] OK Executed $file", PHP_EOL;
-                $stmt = $pdo->prepare("INSERT INTO `migrations` (`type`, `name`) VALUES ('migration', ?)");
-                $stmt->execute([basename($file)]);
-                $stmt->closeCursor();
-            }
-
-            echo "[Migration] OK Migration done", PHP_EOL;
-        } catch (Exception) {
-            echo "[Migration] FAILED Migrations: ", $e->getMessage(), PHP_EOL;
-        }
+        echo cmd(['migration_start']);
     }
-} catch (Exception) {
+} catch (Exception $e) {
     echo "[Migration] FAILED ", $e->getMessage(), PHP_EOL;
 }

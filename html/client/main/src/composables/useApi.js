b5e408e1d482d7cff45f4d1de36e4ce5298ad5a5
eru123 <yeoligoakino@gmail.com>
2023-09-04 23:57:46 +0800

    added login api

--- html/client/main/src/composables/useApi.js
+++ html/client/main/src/composables/useApi.js
@@ -5,55 +5,71 @@ export const $server = useServerData();
 export const localData = usePersistentData("user", null);
 
 export const post = (url, data) => {
-    const url_obj = new URL(url, BASE_URL);
-    return fetch(url_obj, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-    }).then((res) => res.json());
+  const url_obj = new URL(url, BASE_URL);
+  return fetch(url_obj, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((res) => res.json());
 };
 
 export const get = (url, data) => {
-    const url_obj = new URL(url, BASE_URL);
-    for (const key in data) {
-        url_obj.searchParams.append(key, data[key]);
-    }
-    return fetch(url_obj).then((res) => res.json());
+  const url_obj = new URL(url, BASE_URL);
+  for (const key in data) {
+    url_obj.searchParams.append(key, data[key]);
+  }
+  return fetch(url_obj).then((res) => res.json());
 };
 
 export const loginWithData = (token, data) => {
-    localData.value = data;
-    localData.value.token = token;
+  localData.value = data;
+  localData.value.token = token;
 };
 
 export const logout = () => {
-    localData.value = null;
+  localData.value = null;
 };
 
 export const register = (data) => {
-    return post("/api/v1/auth/register", data)
-        .then((res) => {
-            if (res?.error) {
-                throw new Error(res.error);
-            }
+  return post("/api/v1/auth/register", data)
+    .then((res) => {
+      if (res?.error) {
+        throw new Error(res.error);
+      }
 
-            if (res?.token && res?.data) {
-                loginWithData(res.token, res.data);
-                return res;
-            }
+      if (res?.token && res?.data) {
+        loginWithData(res.token, res.data);
+        return res;
+      }
 
-            throw new Error("Invalid server response");
-        })
-        .catch((err) => {
-            createError("Registration Error", err?.message);
-            return null;
-        })
+      throw new Error("Invalid server response");
+    })
+    .catch((err) => {
+      createError("Registration Error", err?.message);
+      return null;
+    });
+};
+
+export const login = (data) => {
+  return post("/api/v1/auth/login", data)
+    .then((res) => {
+      if (res?.token && res?.data) {
+        loginWithData(res.token, res.data);
+        return res;
+      }
+
+      throw new Error("Invalid server response");
+    })
+    .catch((err) => {
+      createError("Login Error", err?.message);
+      return null;
+    });
 };
 
 export default {
-    data: localData,
-    post,
-    get,
+  data: localData,
+  post,
+  get,
 };

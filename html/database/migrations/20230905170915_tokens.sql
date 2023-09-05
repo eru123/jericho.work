3fce301da580d5dc4b7bce86a580c5be28ec44fc
ipp-jericho <jericho@payshield.ai>
2023-09-05 17:28:23 +0800

    update database

--- html/database/migrations/20230905170915_tokens.sql
+++ html/database/migrations/20230905170915_tokens.sql
@@ -1,10 +1,9 @@
 CREATE TABLE `tokens` (
-	`token` TEXT NOT NULL COLLATE 'utf8mb4_bin',
+	`token` VARCHAR(512) NOT NULL COLLATE 'utf8mb4_bin',
 	`user_id` INT(10) NULL DEFAULT NULL,
 	`type` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_bin',
 	`expired_at` DATETIME NULL DEFAULT NULL,
-    INDEX `tokens_type_index` (`type`),
+	INDEX `tokens_type_index` (`type`),
 	INDEX `tokens_user_id_index` (`user_id`),
 	INDEX `tokens_token_index` (`token`)
-)
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;

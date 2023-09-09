0e97835400776834572f7ab6e4a7d21d3903e582
eru123 <yeoligoakino@gmail.com>
2023-09-09 23:54:38 +0800

    global redirect

--- html/client/main/src/App.vue
+++ html/client/main/src/App.vue
@@ -3,9 +3,17 @@ import { watchEffect } from "vue";
 import useServerData from "@/composables/useServerData";
 import ServerError from "@/views/ServerError.vue";
 import Dialogs from "@/components/Dialogs.vue";
+import { useRouter } from "vue-router";
 
 const $server = useServerData();
+const router = useRouter();
+
 watchEffect(() => {
+    window.__skiddph__redirect = (path) => {
+        window.scrollTo(0, 0);
+        router.push(path);
+    }
+
     if ($server?.debug) {
         console.log("Server Data", $server);
         if ($server?.error) {

164b2f156db63d42bf6a0b14c87a3c5f5dc9d8c0
eru123 <yeoligoakino@gmail.com>
2023-09-17 20:44:24 +0800

    update

--- html/client/main/src/composables/useWebsocket.js
+++ html/client/main/src/composables/useWebsocket.js
@@ -6,29 +6,94 @@ const $server = useServerData();
 const $user = usePersistentData("user", null);
 
 var ws;
+var callbacks = [];
 
 export const send = (data) => {
     ws.send(JSON.stringify(data));
 };
 
 export const useWebsocket = () => {
-    const token = $user?.value?.token || "secretJWTToken";
-    ws = new WebSocket($server.value?.WS_HOST);
+    if (ws) return ws;
 
-    ws.onopen = () => send({ action: "auth", token });
+    const token = $user?.value?.token || "secretJWTToken";
+    ws = new WebSocket($server?.WS_HOST);
+
+    ws.onopen = () => {
+        send({ action: "auth", token });
+        callbacks.forEach(({ event, callback }) => {
+            if (event === "open") callback();
+        });
+    }
+
     ws.onmessage = (e) => {
-        const data = JSON.parse(e.data);
-        if (data?.type === "error") {
-            console.log("WS Error: ", data);
+        try {
+            const data = JSON.parse(e.data);
+
+            callbacks.forEach(({ event, callback }) => {
+                if (event === "message") callback(data);
+            });
+
+            if (data?.event) {
+                callbacks.forEach(({ event, callback }) => {
+                    if (event === data.event) callback(data);
+                });
+            }
+            if (data?.type === "error") {
+                callbacks.forEach(({ event, callback }) => {
+                    if (event === "error") callback(data);
+                });
+            }
+        } catch (e) {
+            report("error", {
+                message: e?.message || "Unknown error",
+                origin: "useWebsocket.js - onmessage",
+                data: e?.data,
+                error: e
+            })
+            callbacks.forEach(({ event, callback }) => {
+                if (event === "error") callback(e);
+            });
         }
     };
 
-    ws.onclose = () => ws = null;
+    ws.onclose = () => {
+        callbacks.forEach(({ event, callback }) => {
+            if (event === "close") callback();
+        });
+        ws = null;
+    }
     ws.onerror = (e) => {
-        report("error", e);
+        report("error", {
+            message: e?.message || "Unknown error",
+            origin: "useWebsocket.js - onerror",
+            data: e?.data,
+            error: e
+        })
+        callbacks.forEach(({ event, callback }) => {
+            if (event === "error") callback(e);
+        });
         if (ws) ws.close();
         ws = null;
     }
 
     return ws;
 };
+
+export default useWebsocket;
+export const useWS = useWebsocket;
+export const close = () => {
+    if (ws) ws.close();
+    ws = null;
+}
+export const restart = () => {
+    if (ws) ws.close();
+    ws = null;
+    return useWebsocket();
+}
+
+export const subscribe = (channel) => send({ action: "subscribe", channel });
+export const unsubscribe = (channel) => send({ action: "unsubscribe", channel });
+
+export const on = (event, callback) => {
+    callbacks.push({ event, callback });
+}

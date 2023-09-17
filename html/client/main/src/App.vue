28de16071eb59eeae888c6854d9bfccc2283b007
eru123 <yeoligoakino@gmail.com>
2023-09-17 23:31:20 +0800

    update

--- html/client/main/src/App.vue
+++ html/client/main/src/App.vue
@@ -4,7 +4,6 @@ import { auth_init } from "@/composables/useApi";
 import ServerError from "@/views/ServerError.vue";
 import Dialogs from "@/components/Dialogs.vue";
 import { useRouter } from "vue-router";
-import { useWebsocket, on as onWs } from "@/composables/useWebsocket";
 
 const $server = useServerData();
 const router = useRouter();
@@ -22,31 +21,6 @@ if ($server?.debug) {
 }
 
 auth_init("/");
-useWebsocket();
-
-onWs("open", () => {
-  console.log("Websocket connected");
-});
-
-onWs("message", (data) => {
-  console.log("Server sent a message", data);
-});
-
-onWs("close", (data) => {
-  console.log("Websocket closed", data);
-  window.__skiddph__retry = true;
-  var timeout = 1000;
-  var retry = setInterval(() => {
-    console.log("Retrying websocket connection");
-    if (window.__skiddph__retry) {
-      clearInterval(retry);
-      window.__skiddph__retry = false;
-      timeout *= 2;
-      useWebsocket();
-    }
-  }, 1000);
-});
-
 </script>
 <template>
   <ServerError v-if="$server?.error" />

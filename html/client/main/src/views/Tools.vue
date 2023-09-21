26ceb63d077c7070326f76bb98a0b46cf0ca207f
eru123 <yeoligoakino@gmail.com>
2023-09-21 17:43:20 +0800

    added tools collection search

--- html/client/main/src/views/Tools.vue
+++ html/client/main/src/views/Tools.vue
@@ -30,7 +30,7 @@ const all_tools = {
       name: "JSON Web Token",
       icon: "bi-file-earmark-lock-fill",
       url: "/tools/json-web-token",
-    }
+    },
   ],
 };
 
@@ -41,6 +41,9 @@ const tools = computed(() => {
       filtered_tools[category] = tools.filter((tool) =>
         tool.name.toLowerCase().includes(search.value.toLowerCase())
       );
+      if (filtered_tools[category].length === 0) {
+        delete filtered_tools[category];
+      }
     }
     return filtered_tools;
   } else {
@@ -51,10 +54,11 @@ const tools = computed(() => {
 <template>
   <v-public-page>
     <div class="container">
-      <h1>Tools Collection</h1>
-      <div class="row">
-        <div class="col-12 col-md-6">
-          <v-input v-model="search" placeholder="Search"></v-input>
+      <div class="header">
+        <h1>Tools Collection</h1>
+        <div class="form-control">
+          <label>Search</label>
+          <input v-model="search" type="text">
         </div>
       </div>
       <div class="group" v-for="(items, group) of tools" :key="group">
@@ -78,19 +82,35 @@ const tools = computed(() => {
 .container {
   @apply w-full max-w-screen-xl mx-auto p-4;
 
-  h1 {
-    @apply text-3xl mb-4 text-gray-800;
+  .header {
+    @apply flex flex-col md:flex-row md:justify-between md:items-center md:py-4;
+
+    h1 {
+      @apply text-2xl mb-4 md:mb-0 text-gray-800;
+    }
+
+    .form-control {
+      @apply md:w-fit w-full flex items-center py-0 mb-4 md:mb-0;
+
+      label {
+        @apply pl-4 pr-2 border-l border-t border-b md:py-1 py-2 rounded-l-md bg-gray-100;
+      }
+
+      input {
+        @apply flex-1 md:py-1 p-2 border rounded-r;
+      }
+    }
   }
 
   .group {
     @apply mb-4;
 
     h3 {
-      @apply text-2xl mb-2 text-gray-700;
+      @apply text-xl mb-2 text-gray-700;
     }
 
     .items {
-      @apply grid grid-cols-1 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-2;
+      @apply grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-2;
 
       .item {
         @apply flex items-center p-2 text-primary-800 hover:bg-primary-50 rounded-md;

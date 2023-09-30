15314bd11b9e3bff3079a32e1a49da224127f987
eru123 <yeoligoakino@gmail.com>
2023-09-30 12:19:18 +0800

    mailtpl: init mail verification

---
+++ html/mailtpl/newsletter-add.php
@@ -0,0 +1,18 @@
+<?php
+
+return <<<TEMPLATE
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <p>Hi there!</p>
+    <p>Your email address (\${ email }) has been used to subscribe to our newsletter.</p>
+    <p>To verify your email address, please click or copy and paste the link below to your browser:</p>
+    <p><a href="\${ link }">\${ link }</a></p>
+    <p>Best regards,<br>SKIDD PH</p>
+    <br>
+    <br>
+    <p><small>This is an automated message. Please do not reply to this email.</small></p>
+    <p><small>If you did not register on our site using this email address, please ignore this email.</small></p>
+</body>
+</html>
+TEMPLATE;

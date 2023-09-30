b4b1e643f6ed7b82e603569898c925eefa1aa849
eru123 <yeoligoakino@gmail.com>
2023-09-30 12:22:52 +0800

    model: update newsletter config

--- html/App/Models/Newsletter.php
+++ html/App/Models/Newsletter.php
@@ -5,15 +5,21 @@ namespace App\Models;
 class Newsletter extends AbstractModel
 {   
     protected static $table = 'newsletter';
-    protected static $allowed = [];
-    protected static $created_at = false;
-    protected static $updated_at = false;
-    protected static $deleted_at = false;
-    protected static $disabled_at = false;
-    protected static $primary_key = false;
+    protected static $allowed = [
+        'email',
+        'subscriptions',
+        'verified',
+        'disabled_at'
+    ];
+
+    protected static $created_at = 'created_at';
+    protected static $updated_at = 'updated_at';
+    protected static $deleted_at = 'deleted_at';
+    protected static $disabled_at = 'disabled_at';
+    protected static $primary_key = 'id';
 
     protected static $date_format = 'Y-m-d H:i:s';
-    protected static $soft_delete = false;
-    protected static $use_created_at = false;
-    protected static $use_updated_at = false;
+    protected static $soft_delete = true;
+    protected static $use_created_at = true;
+    protected static $use_updated_at = true;
 }

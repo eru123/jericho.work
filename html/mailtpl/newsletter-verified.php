f20d027a5d54e3c13dbde3b90a3277bc71153cb4
eru123 <yeoligoakino@gmail.com>
2023-09-30 12:20:01 +0800

    mailtpl: init approve newsletter

---
+++ html/mailtpl/newsletter-verified.php
@@ -0,0 +1,17 @@
+<?php
+
+return <<<TEMPLATE
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <p>Hi there!</p>
+    <p>Thank you for subscribing to our newsletter!<br />
+        Your email address (\${ email }) has been verified.</p>
+    <p>Best regards,<br>SKIDD PH</p>
+    <br>
+    <br>
+    <p><small>This is an automated message. Please do not reply to this email.</small></p>
+    <p><small>If you did not register on our site using this email address, please ignore this email.</small></p>
+</body>
+</html>
+TEMPLATE;

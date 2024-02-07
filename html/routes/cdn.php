acb9166438e102ce91cbd0ef274a4f7c97078c2a
eru123 <yeoligoakino@gmail.com>
2024-02-07 17:50:50 +0800

    rm: cdn domain routes

--- html/routes/cdn.php
+++ html/routes/cdn.php
@@ -3,20 +3,20 @@
 use eru123\router\Router;
 
 $cdn = new Router();
-$cdn->base('/cdn');
-$cdn->error(function (Throwable $e) {
-    $code = intval($e->getCode() ?: 500);
-    return [
-        'status' => false,
-        'code' => $code,
-        'error' => $e->getMessage()
-    ];
-});
+// $cdn->base('/cdn');
+// $cdn->error(function (Throwable $e) {
+//     $code = intval($e->getCode() ?: 500);
+//     return [
+//         'status' => false,
+//         'code' => $code,
+//         'error' => $e->getMessage()
+//     ];
+// });
 
-$cdn->post('/upload', 'App\Controller\CDN@upload');
-$cdn->get('/stream/$id/$name', 'App\Controller\CDN@stream');
-$cdn->get('/download/$id/$name', 'App\Controller\CDN@download');
-$cdn->get('/', 'App\Controller\CDN@index');
-$cdn->static('/', [__CLIENT__ . '/cdn/dist/cdn'], [], 'App\Controller\CDN::index');
+// $cdn->post('/upload', 'App\Controller\CDN@upload');
+// $cdn->get('/stream/$id/$name', 'App\Controller\CDN@stream');
+// $cdn->get('/download/$id/$name', 'App\Controller\CDN@download');
+// $cdn->get('/', 'App\Controller\CDN@index');
+// $cdn->static('/', [__CLIENT__ . '/cdn/dist/cdn'], [], 'App\Controller\CDN::index');
 
 return $cdn;

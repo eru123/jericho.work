f6606d7b6caf5ad1645861c1b1badc53226327f8
eru123 <yeoligoakino@gmail.com>
2024-02-14 04:54:02 +0800

    imp: user layout

--- client/src/router/user.js
+++ client/src/router/user.js
@@ -1,7 +1,18 @@
 export default [
     {
-        path: '/profile',
-        name: 'Profile',
-        component: () => import('@/views/Profile.vue')
+        path: '/',
+        name: 'UserLayout',
+        component: () => import('@/layouts/UserLayout.vue'),
+        children: [
+            {
+                path: 'profile',
+                name: 'Profile',
+                component: () => import('@/views/Profile.vue')
+            },
+            {
+                path: 'dashboard',
+                name: 'Dashboard',
+            }
+        ]
     }
 ]

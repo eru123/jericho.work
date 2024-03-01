70e637d992fa608d5563f8d19c20dc75e8cb4168
eru123 <yeoligoakino@gmail.com>
2024-03-01 19:51:34 +0800

    up: improve

--- client/src/components/FixedLayout.vue
+++ client/src/components/FixedLayout.vue
@@ -1,69 +1,82 @@
 <script setup>
-import { ref, watchEffect, computed } from 'vue'
-import usePersistentData from '@/composables/usePersistentData';
+import { ref, watchEffect, computed } from "vue";
+import usePersistentData from "@/composables/usePersistentData";
 import PublicHeader from "@/components/PublicHeader.vue";
 
-const props = defineProps([
-    'sidebar',
-    'pageName'
-])
+const props = defineProps(["sidebar", "pageName"]);
 
-const sidebarOpen = usePersistentData('sidebarOpen', null)
-const sidebarFirstLoad = ref(true)
+const sidebarOpen = usePersistentData("sidebarOpen", null);
+const sidebarFirstLoad = ref(true);
 
 const toggleSidebar = () => {
     sidebarFirstLoad.value = false;
     sidebarOpen.value = !sidebarOpen.value;
-}
+};
 
-const showSidebar = () => props?.sidebar && !Array.isArray(props.sidebar) && typeof props.sidebar === 'object' && props.sidebar !== null;
+const showSidebar = () =>
+    props?.sidebar &&
+    !Array.isArray(props.sidebar) &&
+    typeof props.sidebar === "object" &&
+    props.sidebar !== null;
 
 const sidebarClass = computed(() => {
-    const classes = ['side-navigation'];
+    const classes = ["side-navigation"];
 
-    if (!sidebarOpen.value && sidebarOpen.value === null && sidebarFirstLoad.value) {
-        classes.push('hide')
+    if (
+        !sidebarOpen.value &&
+        sidebarOpen.value === null &&
+        sidebarFirstLoad.value
+    ) {
+        classes.push("hide");
     }
 
-    if (sidebarOpen.value !== null && !sidebarOpen.value && !sidebarFirstLoad.value) {
-        classes.push('hide')
-        classes.push('slide-in-left')
+    if (
+        sidebarOpen.value !== null &&
+        !sidebarOpen.value &&
+        !sidebarFirstLoad.value
+    ) {
+        classes.push("hide");
+        classes.push("slide-in-left");
     }
 
     if (sidebarOpen.value && !sidebarFirstLoad.value) {
-        classes.push('slide-in-right')
+        classes.push("slide-in-right");
     }
 
-    return classes.join(' ');
+    return classes.join(" ");
 });
 
 watchEffect(() => {
     if (sidebarOpen.value === null) {
-        sidebarOpen.value = window.innerWidth >= 768
-        sidebarFirstLoad.value = true
+        sidebarOpen.value = window.innerWidth >= 768;
+        sidebarFirstLoad.value = true;
     } else {
         sidebarFirstLoad.value = false;
     }
-})
+});
 
 window?.addEventListener("resize", () => {
     sidebarFirstLoad.value = false;
-    sidebarOpen.value = window.innerWidth >= 768
+    sidebarOpen.value = window.innerWidth >= 768;
 });
-
 </script>
 <template>
     <PublicHeader />
     <div class="fixed-layout-container">
         <div :class="sidebarClass" v-if="showSidebar">
-            <v-link v-for="item in props.sidebar" :key="item.name" :to="item.to ? item.to : '#'" class="parent-item">{{
-                item.name }}</v-link>
+            <v-link v-for="item in props.sidebar" :key="item.name" :to="item.to ? item.to : ''"
+                :class="item.heading ? 'parent-heading' : 'parent-item'">
+                {{ item.name }}
+            </v-link>
         </div>
         <div class="content">
             <div class="content-header">
-                <button v-if="showSidebar" @click="toggleSidebar" class="sidebar-toggle"><v-icon
-                        name="hi-solid-menu-alt-2"></v-icon></button>
-                <span v-if="props?.pageName" class="content-title">{{ props?.pageName }}</span>
+                <button v-if="showSidebar" @click="toggleSidebar" class="sidebar-toggle">
+                    <v-icon name="hi-solid-menu-alt-2"></v-icon>
+                </button>
+                <span v-if="props?.pageName" class="content-title">{{
+                    props?.pageName
+                }}</span>
             </div>
             <slot></slot>
         </div>
@@ -73,7 +86,6 @@ window?.addEventListener("resize", () => {
 .fixed-layout-container {
     @apply w-screen min-h-[calc(100vh-3.5rem)] flex flex-row;
 
-
     .side-navigation {
         @apply flex h-[calc(100vh-3.5rem)] bg-primary-900 p-2 min-w-[220px] text-primary-50 flex-col overflow-y-auto transition-all duration-200 translate-x-0;
 
@@ -82,7 +94,11 @@ window?.addEventListener("resize", () => {
         }
 
         .parent-item {
-            @apply py-2 px-4 rounded-md hover:bg-primary-800;
+            @apply py-2 md:py-1 px-4 rounded-md hover:bg-primary-800;
+        }
+
+        .parent-heading {
+            @apply py-1 pl-2 pr-6 -mx-2 my-2 rounded-none text-sm bg-primary-800 text-primary-50 cursor-default;
         }
     }
 
@@ -104,8 +120,6 @@ window?.addEventListener("resize", () => {
                 }
             }
         }
-
-
     }
 
     .icon {

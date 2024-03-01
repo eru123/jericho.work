0dff06463cd3998a75b19853b369ef543e43a25c
eru123 <yeoligoakino@gmail.com>
2024-03-01 19:53:02 +0800

    up: make UserLayout fixed component

--- client/src/router/user.js
+++ client/src/router/user.js
@@ -1,18 +1,21 @@
+import UserLayout from '@/layouts/UserLayout.vue'
+
 export default [
     {
         path: '/',
         name: 'UserLayout',
-        component: () => import('@/layouts/UserLayout.vue'),
+        component: UserLayout,
         children: [
             {
                 path: 'profile',
                 name: 'Profile',
                 component: () => import('@/views/Profile.vue')
             },
-            {
-                path: 'dashboard',
-                name: 'Dashboard',
-            }
+            // {
+            //     path: 'dashboard',
+            //     name: 'Dashboard',
+            //     component: () => import('@/views/Dashboard.vue')
+            // }
         ]
     }
 ]
